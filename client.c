147a46fbb8437df77f0acb490cdea16962366428
loicchidlord <66997379+loicchidlord@users.noreply.github.com>
2020-06-19 13:14:44 +0200

    Update client.c

--- client.c
+++ client.c
@@ -19,7 +19,7 @@ int main(void)
 {
 	int socketClient = socket(AF_INET, SOCK_STREAM, 0);
 	struct sockaddr_in addrClient;
-	addrClient.sin_addr.s_addr = inet_addr("192.168.1.89");
+	addrClient.sin_addr.s_addr = inet_addr("127.0.0.1");
 	addrClient.sin_family = AF_INET;
 	addrClient.sin_port = htons(30000);
 	connect(socketClient, (const struct sockaddr *)&addrClient, sizeof(addrClient));
